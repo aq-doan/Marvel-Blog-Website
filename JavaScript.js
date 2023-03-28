8186a54d23b583c3046456ca7ebe07df589d6128
Opherus <jonathan.charleston.00@gmail.com>
2023-03-28 22:33:04 +1100

    Added extra element: button to scroll to the top.

--- JavaScript.js
+++ JavaScript.js
@@ -22,3 +22,23 @@ function hideContent(initiator) {
     initiator.parentElement.style.display = "none";
     initiator.parentElement.parentElement.getElementsByClassName("showmorebutton")[0].style.display = "";
 }
+
+
+/**
+ * Button for scrolling to the top of the page once you've scrolled past a certain point.
+ * Hidden when they are already there.
+ */
+let topButton = document.getElementById("toTopButton");
+
+window.onscroll = function(){
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+};
+
+function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;    
+}
